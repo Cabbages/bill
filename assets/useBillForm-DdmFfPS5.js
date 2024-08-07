86248c4f8f81e66e3f821620e66f4f941b04640d
D.K <840818530@qq.com>
2024-08-07 22:28:43 +0800

    fix: 调整字号

--- assets/useBillForm-DdmFfPS5.js
+++
@@ -1,1 +0,0 @@
-import{n as e,p as r}from"./index-dMH2EGeY.js";const n=e("billForm",()=>({billForm:r({count:6,merchant:"羊城通",merchantFullName:"广州羊城通股份有限公司",paymentTime:"2024年7月16日 18:38:31",paymentType:"零钱",merchandise:"2024-07-16 18:38:31搭乘广州地铁",acquiringMerchant:"财付通支付科技有限公司",trasactionId:"4200002334202407128766580478",merchantId:"2024071220240141j533NOCT"})}));export{n as u};
